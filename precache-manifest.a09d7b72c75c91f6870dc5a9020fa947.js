a0a9fdc3cda6dfd5fd037bd730f01487163063bb
jeanwahlung <jeancarloswahlung@gmail.com>
2019-02-14 15:21:37 -0600

    Updates

---
+++ precache-manifest.a09d7b72c75c91f6870dc5a9020fa947.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4739b23ca5d32b0321e",
+    "url": "/jeanwahlung.github.io/ExamenUX/static/js/runtime~main.e4739b23.js"
+  },
+  {
+    "revision": "9b081c38b0a90eb7b19d",
+    "url": "/jeanwahlung.github.io/ExamenUX/static/js/main.9b081c38.chunk.js"
+  },
+  {
+    "revision": "e270153e0bbe887e55d7",
+    "url": "/jeanwahlung.github.io/ExamenUX/static/js/2.e270153e.chunk.js"
+  },
+  {
+    "revision": "9b081c38b0a90eb7b19d",
+    "url": "/jeanwahlung.github.io/ExamenUX/static/css/main.9d851a67.chunk.css"
+  },
+  {
+    "revision": "e270153e0bbe887e55d7",
+    "url": "/jeanwahlung.github.io/ExamenUX/static/css/2.befe3ce5.chunk.css"
+  },
+  {
+    "revision": "e5a5e1b0230de90f3d903138b48224ac",
+    "url": "/jeanwahlung.github.io/ExamenUX/index.html"
+  }
+];
